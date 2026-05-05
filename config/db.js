033c0d93b127b707e35f9eec271bb99c4f99c4c2
Saurabh Mishra <student001599@gmail.com>
2026-05-06 01:51:08 +0530

    url

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const connectDb=async ()=>{
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect("mongodb+srv://ankitraj1616aug:dS8mzityUo2c39RT@cluster0.yxs41zz.mongodb.net/virtualassistent?appName=Cluster0")
         console.log("db connected")
     } catch (error) {
         console.log(error)
